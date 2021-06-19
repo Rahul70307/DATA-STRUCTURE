fc0ad95bd3c06e52d12cc649fc2580ffff93d75f
rahul70307 <rahul703nitp@gmail.com>
2021-06-20 03:44:42 +0530

    optimised single Linked list and added the delete functionality

--- linkedList/SingleLinkedList.java
+++ linkedList/SingleLinkedList.java
@@ -26,41 +26,134 @@ public class SingleLinkedList {
         return  head;
 
     }
-    public void insertInLinkedList(int nodeValue, int location){
+//    public void insertInLinkedList1(int nodeValue, int location){
+//        SingleNode node= new SingleNode();
+//        node.setValue( nodeValue);
+//        node.setNext(null);
+//        if(location ==1){
+//            node.setNext(head);
+//            head= node;
+//        }
+//        else if (location >getSize() ){
+//            tail.setNext(node);
+//            tail= node;
+//        }
+//        else{
+//            SingleNode tmp = head;
+//            int counter = 0;
+//            while( counter< location-2){
+//                tmp = tmp.getNext();
+//                counter++;
+//            }
+//            node.setNext(tmp.getNext());
+//            tmp.setNext(node);
+//        }
+//        setSize(getSize()+1);
+//
+//    }
+    public void insertInLikedList(int nodeValue, int location){
         SingleNode node= new SingleNode();
         node.setValue( nodeValue);
-        node.setNext(null);
-        if(location ==1){
-            node.setNext(head);
-            head= node;
-        }
-        else if (location >=getSize() ){
-            tail.setNext(node);
-            tail= node;
+
+        if(!isExistsLinkedList()){
+            System.out.println("LinkedList is empty. Creating the first node of linked list");
+            node.setNext(null);
+            head = tail = node;
+            setSize(1);
+
         }
         else{
-            SingleNode tmp = head;
-            int counter = 0;
-            while( counter< location-2){
-                tmp = tmp.getNext();
-                counter++;
+            if(location<=1){
+                node.setNext(head);
+                head=node;
+            }else if(location>= getSize()){
+                node.setNext(null);
+                tail.setNext(node);
+                tail= node;
+            }else{
+                SingleNode currentNode= head;
+                for(int i =1; i<getSize();i++){
+                    currentNode= currentNode.getNext();
+                }
+                node.setNext(currentNode.getNext());
+                currentNode.setNext(node);
             }
-            node.setNext(tmp.getNext());
-            tmp.setNext(node);
+            setSize(getSize()+1);
         }
-        setSize(getSize()+1);
-
     }
     public void traverseLinkedList(){
-        SingleNode current = head;
-        int length = getSize();
-        for( int i =0; i<length;i++){
-            System.out.print(current.getvalue());
-            if(i!=getSize()-1){
-                System.out.print("->");
+        if(!isExistsLinkedList()){
+            System.out.println(" linkedList is empty. Nothing to be deleted!!");
+        }
+        else{
+            SingleNode current = head;
+            int length = getSize();
+            for( int i =0; i<length;i++){
+                System.out.print(current.getvalue());
+                if(i!=getSize()-1){
+                    System.out.print("->");
+                }
+                current= current.getNext();
             }
-            current= current.getNext();
+            System.out.println("");
+
         }
 
+
+    }
+    public boolean isExistsLinkedList(){
+        if( head!=null)
+            return true;
+        else
+            return false;
+    }
+
+    public  void deleteCompleLinkedList(){
+        if(head!=null){
+            head=null;
+            tail=null;
+        }
+    }
+    public void deleteNoteAt(int location){
+        int length= getSize();
+        if(!isExistsLinkedList()){
+            System.out.println("There is not node in the link list. LinkedList is empty");
+        }else{
+            if (location==1){
+                if(head==tail){       //there is only one node condition
+                    head= null;
+                    tail=null;
+                }else{
+                    head=head.getNext();
+                }
+            }
+            else if(location>=length){
+                System.out.println("Entered location to be deleted is "+ location+" and the size of liked list is "+ getSize()+ " So deleting last node");
+                if(head==tail){       //there is only one node condition
+                    head= null;
+                    tail=null;
+                }
+                SingleNode currentNode= head;
+                SingleNode previousNode= null;
+                while(currentNode.getNext()!=null){
+                    previousNode=currentNode;
+                    currentNode= currentNode.getNext();
+                }
+                System.out.println("deleting value at location = "+ location+ " is "+ currentNode.getvalue());
+                previousNode.setNext(null);
+                tail=previousNode;
+
+            }else if (location<getSize()){
+                SingleNode currentNode= head;
+                SingleNode previousNode= null;
+                for(int i=1; i<location;i++){
+                    previousNode=currentNode;
+                    currentNode= currentNode.getNext();
+                }
+                System.out.println("deleting value at location = "+ location+ " is "+ currentNode.getvalue());
+                previousNode.setNext(currentNode.getNext());
+            }
+            setSize(getSize()-1);
+        }
     }
 }
--- linkedList/SingleLinkedListMain.java
+++ linkedList/SingleLinkedListMain.java
@@ -3,17 +3,31 @@ package linkedList;
 public class SingleLinkedListMain {
     public static void main( String[] args){
         SingleLinkedList list= new SingleLinkedList();
-        list.createSingleLinkedList(1);
-        list.insertInLinkedList(2,2);
-        list.insertInLinkedList(4,3);
-        list.insertInLinkedList(5,4);
-        list.insertInLinkedList(3,3);
-        list.insertInLinkedList(0,1);
-        list.insertInLinkedList(-1,1);
-        list.insertInLinkedList(-2,1);
-        list.insertInLinkedList(9,19);
-
-
+        list.insertInLikedList(2,2);
         list.traverseLinkedList();
+        list.insertInLikedList(1,1);
+        list.traverseLinkedList();
+        list.insertInLikedList(3,3);
+        list.traverseLinkedList();
+        list.insertInLikedList(5,10);
+        list.traverseLinkedList();
+        list.insertInLikedList(4,4);
+        list.traverseLinkedList();
+        list.insertInLikedList(6,7);
+        list.traverseLinkedList();
+        list.insertInLikedList(0,0);
+        list.traverseLinkedList();
+        list.insertInLikedList(-1,0);
+        list.traverseLinkedList();
+        list.insertInLikedList(7,8);
+        list.traverseLinkedList();
+        list.deleteCompleLinkedList();
+        list.traverseLinkedList();
+
+
+
+
+
+
     }
 }
