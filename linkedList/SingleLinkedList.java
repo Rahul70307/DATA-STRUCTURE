4a8a1dede26e08f8c0d8cb08bea43289e47f94ac
rahul70307 <rahul703nitp@gmail.com>
2021-06-27 00:23:15 +0530

    implementation of Single linked List

--- linkedList/SingleLinkedList.java
+++ linkedList/SingleLinkedList.java
@@ -8,68 +8,68 @@ public class SingleLinkedList {
     private SingleNode tail;
     private int size;
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
-    public void setSize(int size){
+
+    public void setSize(int size) {
         this.size = size;
 
     }
 
-    public SingleNode createSingleLinkedList(int nodeValue){
+    public SingleNode createSingleLinkedList(int nodeValue) {
         SingleNode node = new SingleNode();
         node.setValue(nodeValue);
         node.setNext(null);
-        head= node;
-        tail=node;
-        size=1;
-        return  head;
+        head = node;
+        tail = node;
+        size = 1;
+        return head;
 
     }
 
-    public void insertInLikedList(int nodeValue, int location){
-        SingleNode node= new SingleNode();
-        node.setValue( nodeValue);
+    public void insertInLikedList(int nodeValue, int location) {
+        SingleNode node = new SingleNode();
+        node.setValue(nodeValue);
 
-        if(!isExistsLinkedList()){
+        if (!isExistsLinkedList()) {
             System.out.println("LinkedList is empty. Creating the first node of linked list");
             node.setNext(null);
             head = tail = node;
             setSize(1);
 
-        }
-        else{
-            if(location<=1){
+        } else {
+            if (location <= 1) {
                 node.setNext(head);
-                head=node;
-            }else if(location>getSize()){
+                head = node;
+            } else if (location > getSize()) {
                 node.setNext(null);
                 tail.setNext(node);
-                tail= node;
-            }else{
-                SingleNode currentNode= head;
-                for(int i =1; i<location-1;i++){
-                    currentNode= currentNode.getNext();
+                tail = node;
+            } else {
+                SingleNode currentNode = head;
+                for (int i = 1; i < location - 1; i++) {
+                    currentNode = currentNode.getNext();
                 }
                 node.setNext(currentNode.getNext());
                 currentNode.setNext(node);
             }
-            setSize(getSize()+1);
+            setSize(getSize() + 1);
         }
     }
-    public void traverseLinkedList(){
-        if(!isExistsLinkedList()){
+
+    public void traverseLinkedList() {
+        if (!isExistsLinkedList()) {
             System.out.println(" linkedList is empty. Nothing to be deleted!!");
-        }
-        else{
+        } else {
             SingleNode current = head;
             int length = getSize();
-            for( int i =0; i<length;i++){
+            for (int i = 0; i < length; i++) {
                 System.out.print(current.getvalue());
-                if(i!=getSize()-1){
+                if (i != getSize() - 1) {
                     System.out.print("->");
                 }
-                current= current.getNext();
+                current = current.getNext();
             }
             System.out.println("");
 
@@ -77,59 +77,60 @@ public class SingleLinkedList {
 
 
     }
-    public boolean isExistsLinkedList(){
-        if( head!=null)
+
+    public boolean isExistsLinkedList() {
+        if (head != null)
             return true;
         else
             return false;
     }
 
-    public  void deleteCompleLinkedList(){
-        if(head!=null){
-            head=null;
-            tail=null;
+    public void deleteCompleLinkedList() {
+        if (head != null) {
+            head = null;
+            tail = null;
         }
     }
-    public void deleteNodeAt(int location){
-        int length= getSize();
-        if(!isExistsLinkedList()){
-            System.out.println("There is not node in the link list. LinkedList is empty");
-        }else{
-            if (location==1){
-                if(head==tail){       //there is only one node condition
-                    head= null;
-                    tail=null;
-                }else{
-                    head=head.getNext();
-                }
-            }
-            else if(location>=length){
-                System.out.println("Entered location to be deleted is "+ location+" and the size of liked list is "+ getSize()+ " So deleting last node");
-                if(head==tail){       //there is only one node condition
-                    head= null;
-                    tail=null;
-                }
-                SingleNode currentNode= head;
-                SingleNode previousNode= null;
-                while(currentNode.getNext()!=null){
-                    previousNode=currentNode;
-                    currentNode= currentNode.getNext();
-                }
-                System.out.println("deleting value at location = "+ location+ " is "+ currentNode.getvalue());
-                previousNode.setNext(null);
-                tail=previousNode;
 
-            }else if (location<getSize()){
-                SingleNode currentNode= head;
-                SingleNode previousNode= null;
-                for(int i=1; i<location;i++){
-                    previousNode=currentNode;
-                    currentNode= currentNode.getNext();
+    public void deleteNodeAt(int location) {
+        int length = getSize();
+        if (!isExistsLinkedList()) {
+            System.out.println("There is not node in the link list. LinkedList is empty");
+        } else {
+            if (location == 1) {
+                if (head == tail) {       //there is only one node condition
+                    head = null;
+                    tail = null;
+                } else {
+                    head = head.getNext();
                 }
-                System.out.println("deleting value at location = "+ location+ " is "+ currentNode.getvalue());
+            } else if (location >= length) {
+                System.out.println("Entered location to be deleted is " + location + " and the size of liked list is " + getSize() + " So deleting last node");
+                if (head == tail) {       //there is only one node condition
+                    head = null;
+                    tail = null;
+                }
+                SingleNode currentNode = head;
+                SingleNode previousNode = null;
+                while (currentNode.getNext() != null) {
+                    previousNode = currentNode;
+                    currentNode = currentNode.getNext();
+                }
+                System.out.println("deleting value at location = " + location + " is " + currentNode.getvalue());
+                previousNode.setNext(null);
+                tail = previousNode;
+
+            } else if (location < getSize()) {
+                SingleNode currentNode = head;
+                SingleNode previousNode = null;
+                for (int i = 1; i < location; i++) {
+                    previousNode = currentNode;
+                    currentNode = currentNode.getNext();
+                }
+                System.out.println("deleting value at location = " + location + " is " + currentNode.getvalue());
                 previousNode.setNext(currentNode.getNext());
             }
-            setSize(getSize()-1);
+            setSize(getSize() - 1);
         }
     }
 
--- linkedList/SingleLinkedListMain.java
+++ linkedList/SingleLinkedListMain.java
@@ -3,16 +3,14 @@ package linkedList;
 import node.SingleNode;
 
 public class SingleLinkedListMain {
-    public static void main( String[] args) {
+    public static void main(String[] args) {
         SingleLinkedList list = new SingleLinkedList();
         list.insertInLikedList(1, 1);
         list.traverseLinkedList();
         list.insertInLikedList(2, 2);
         list.insertInLikedList(3, 2);
         list.traverseLinkedList();
-
-
     }
 
-    }
+}
 
